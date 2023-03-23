59b2662a60b39a37de838602de3314f5cfafdc91
rooseer._ <roserromancatasus@gmail.com>
2023-03-23 17:00:24 +0100

    [FEAT] creando metodos para buscar pokedom y leer archivo

--- src/main/java/com/mycompany/pokedoms1/Pokedoms1.java
+++ src/main/java/com/mycompany/pokedoms1/Pokedoms1.java
@@ -5,11 +5,15 @@
 package com.mycompany.pokedoms1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -17,25 +21,44 @@ import java.util.Scanner;
  * @author Admin
  */
 public class Pokedoms1 {
+    private static ArrayList<Pokedom> listaPokedoms = new ArrayList<Pokedom>();
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> pokedoms = new ArrayList<String>();
-        
-        
+    public static void main(String[] args) throws IOException 
+        PokedomsBD db = new PokedomsBD(lista);
+        listaPokedoms = new ArrayList<Pokedom>();
         switch(args[1].toLowerCase()){
             case "add":
                 if(args.length ==6){
-                    Pokedom miPokedom = new Pokedom (args[2], args[3],Float.parseFloat(args[4]), Float.parseFloat(args[5]));
-                    FileWriter archivo = new FileWriter(args[0]);
-                    archivo.write("\n" + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-                    archivo.close();
-                    
-
-    
+                    File file = new File(args[0]);
+                    if (file.exists()){
+                        if(db.buscarPokedom(args[2]) == null){
+                            FileWriter archivo = new FileWriter(args[0], true);
+                            archivo.write("\n" + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
+                            archivo.close();
+                            String line = null;
+                            String[] parts = line.split(" ");
+                            Pokedom miPokedom = new Pokedom(parts[0], parts[1], Float.parseFloat(parts[2]), Float.parseFloat(parts[3]));
+                            listaPokedoms.add(miPokedom);
+                        }else{
+                            System.out.println("Este pokedom ya existe");
+                        }
+                    }
                 }
                 break;
             case "describe":
+                if(args.length ==3){
+                    if(db.buscarPokedom(args[2]) != null){
+                            System.out.println("Nom: "+ db.buscarPokedom(args[2]).getNombre());
+                            System.out.println("Familía: "+ db.buscarPokedom(args[2]).getFamilia());
+                            System.out.println("Atac: "+db.buscarPokedom(args[2]).getAtaque());
+                            System.out.println("Defensa: "+db.buscarPokedom(args[2]).getDefensa());
+                       
+                        
+                    }else{
+                        System.out.println("No existe ningun pokedom con ese nombre");
+                    }
+                }
+             
                 break;
             }
         /*String noms[] = new String[] {
--- src/main/java/com/mycompany/pokedoms1/PokedomsBD.java
+++ src/main/java/com/mycompany/pokedoms1/PokedomsBD.java
@@ -5,25 +5,39 @@
  */
 package com.mycompany.pokedoms1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author usuari
  */
 public class PokedomsBD {
-          Pokedom pokedoms[];
-
-    public PokedomsBD(Pokedom[] listaPokedoms) {
-        pokedoms=listaPokedoms;
-    }
-
-    Pokedom describe(String nombrePokedom) {
-        Pokedom elPokedom=null;
-
-        for(Pokedom p: pokedoms) {
-            if (p.getNombre().equalsIgnoreCase(nombrePokedom))
-                elPokedom=p;
+    ArrayList<Pokedom> listaPokedoms = new ArrayList<Pokedom>();
+   
+    public  Pokedom buscarPokedom(String nombre) {
+        for (int i = 0; i < listaPokedoms.size(); i++) {
+            if (listaPokedoms.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                return listaPokedoms.get(i);
+            }
         }
-
-        return elPokedom;
+        return null;
     }  
+    public String leerArchivo(String nombreFichero) throws FileNotFoundException, IOException{
+        File file = new File(nombreFichero);
+            if (file.exists()){
+             FileReader reader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(reader);
+             String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                Pokedom newPokedom = new Pokedom(parts[0], parts[1], Float.parseFloat(parts[2]), Float.parseFloat(parts[3]));
+                listaPokedoms.add(newPokedom);
+            }
+            }
+            }       
 }
